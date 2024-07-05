19e4b4150e7e341d8ca48efe297311e7dc5452c3
andysung <songzongjian0123@163.com>
2024-07-05 11:06:41 +0800

    first commit

---
+++ SwiftUIStudyThree/SwiftUIStudyThreeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIStudyThreeApp.swift
+//  SwiftUIStudyThree
+//
+//  Created by andy on 2024/7/5.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIStudyThreeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
